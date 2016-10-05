f9680b25d5cb9cb54cdd2791d60d47acc3f72242
stef <stef.verve@gmail.com>
2016-10-05 17:44:01 -0400

    Initial Commit

---
+++ Toyotas are Cars too/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Toyotas are Cars too
+//
+//  Created by Stefan Verveniotis on 2016-10-05.
+//  Copyright © 2016 Stefan Verveniotis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
